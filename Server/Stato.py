a002599049bfa9299519f5650695653cff8d39d8
panqifan <panqifanandrea@gmail.com>
2022-07-20 12:06:39 +0200

    prova

---
+++ Server/Stato.py
@@ -0,0 +1,19 @@
+class Stato:
+    def __init__(self):
+        pass
+
+    def __init__(self, Stato):
+        pass
+
+    #return: Stato attuale -> str
+    def getStatoAttuale(self):
+        pass
+    
+    #return: null se untente non logato, atrimenti il suo apikey -> str
+    def getApiKey(self): 
+        pass
+
+    #return: i dati delle conversazioni precedenti -> dict
+    def getDati(self): 
+        pass
+    
---
+++ Server/prova.py
@@ -0,0 +1,7 @@
+import StatementStato
+import Stato
+import StatoPresenzaSede
+
+s= StatoPresenzaSede()
+x=StatementStato("ciao", s)
+x.prova()
