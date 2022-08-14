9c99265489804a9cb94ff55aaa0163c3f80ca2d0
TommasoBerlaffa <83396379+TommasoBerlaffa@users.noreply.github.com>
2022-08-14 20:11:26 +0200

    React Testing

--- Client/src/App.test.js
+++ Client/src/App.test.js
@@ -1,8 +1,45 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('test main chat render', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Benvenuto su Bot4Me, sarò il tuo assistente personale 😄/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('test main chat info 1 render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Posso aiutarti a semplificare i tuoi compiti aziendali come:/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test main chat info 3 render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Registrare la tua presenza in sede/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test main chat info 2 render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Inserire rendiconto ore/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test main chat info 4 render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Aprire il cancello aziendale/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test main chat info 5 render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/E molto altro .../i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('test main chat info 6 render', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Mettimi alla prova!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
