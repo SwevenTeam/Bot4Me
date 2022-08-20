47020b11ddf1840ba44e789b91d53094c419bb9d
TommasoBerlaffa <TommasoBerlaffa@users.noreply.github.com>
2022-08-20 16:23:33 +0000

    autopep8 action fixes

--- Server/Adapter/Test/test_Adapter_Gate.py
+++ Server/Adapter/Test/test_Adapter_Gate.py
@@ -7,6 +7,7 @@ from State.State_Gate import State_Gate
 from State.State_Presence import State_Presence
 from sqlalchemy import null
 
+
 class Test_Adapter_Gate():
 
     # Creazione Chatbot Temporaneo per Test
@@ -24,7 +25,10 @@ class Test_Adapter_Gate():
 
     # Test Apertura Cancello Sede
     def test_Adapter_Gate_Area(self, chatbot):
-        S = Statement_State("imola", State_Gate(),'12345678-1234-1234-1234-123456789012')
+        S = Statement_State(
+            "imola",
+            State_Gate(),
+            '12345678-1234-1234-1234-123456789012')
         A = Adapter_Gate(chatbot)
         if A.can_process(S):
             value = A.process(S, None)
@@ -33,8 +37,11 @@ class Test_Adapter_Gate():
     # Test Apertura Cancello Modifica Sede
     def test_Adapter_Gate_Modify(self, chatbot):
         Sg = State_Gate()
-        Sg.addData("sede","imola")
-        S = Statement_State("modifica", Sg,'12345678-1234-1234-1234-123456789012')
+        Sg.addData("sede", "imola")
+        S = Statement_State(
+            "modifica",
+            Sg,
+            '12345678-1234-1234-1234-123456789012')
         A = Adapter_Gate(chatbot)
         if A.can_process(S):
             value = A.process(S, None)
@@ -43,19 +50,22 @@ class Test_Adapter_Gate():
     # Test Apertura Cancello conferma errore
     def test_Adapter_Gate_Error(self, chatbot):
         Sg = State_Gate()
-        Sg.addData("sede","imola")
-        S = Statement_State("conferma", Sg,'12')
+        Sg.addData("sede", "imola")
+        S = Statement_State("conferma", Sg, '12')
         A = Adapter_Gate(chatbot)
         if A.can_process(S):
             value = A.process(S, None)
         assert value.text == "Si è verificato un errore sconosciuto, riprova a confermare"
 
-
     # Test Apertura Cancello conferma ancora
+
     def test_Adapter_Gate_Confirm_Again(self, chatbot):
         Sg = State_Gate()
-        Sg.addData("sede","imola")
-        S = Statement_State("asdasdasdsadsad", Sg,'12345678-1234-1234-1234-123456789012')
+        Sg.addData("sede", "imola")
+        S = Statement_State(
+            "asdasdasdsadsad",
+            Sg,
+            '12345678-1234-1234-1234-123456789012')
         A = Adapter_Gate(chatbot)
         if A.can_process(S):
             value = A.process(S, None)
@@ -69,6 +79,7 @@ class Test_Adapter_Gate():
         assert value == "Sede accettata : Richiesta apertura del cancello avvenuta con successo"
     '''
     # Test Apertura Cancello Sede Incorretta
+
     def test_Adapter_Gate_Location_Incorrect(self, chatbot):
         S = Statement_State(
             "Imolaaaaaa",
@@ -79,19 +90,18 @@ class Test_Adapter_Gate():
             value = A.process(S, None)
         assert value.text == "Sede non trovata : Reinserire la sede del cancello"
 
-
     # Test is Ready No Api
+
     def test_Adapter_Gate_Is_Ready_Error(self, chatbot):
-        S = Statement_State("ciao", State_Null(),'')
+        S = Statement_State("ciao", State_Null(), '')
         A = Adapter_Gate(chatbot)
         assert A.can_process(S) == False
 
     # Test Apertura Cancello conferma ancora
     def test_Adapter_Gate_Is_Ready_False(self, chatbot):
         Sp = State_Presence()
-        Sp.addData("sede","imola")
-        S = Statement_State("conferma", Sp,'')
+        Sp.addData("sede", "imola")
+        S = Statement_State("conferma", Sp, '')
         A = Adapter_Gate(chatbot)
         value = A.process(S, None)
         assert value.text == "Dati non inseriti correttamente, provare a modificare e reinserire"
-       
